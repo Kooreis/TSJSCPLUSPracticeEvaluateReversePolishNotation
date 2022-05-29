81380b902d28ebfbee93e83a1ae4b616d8f365fe
Kooreis <kooreis.official@gmail.com>
2022-05-29 12:39:58 -0500

    Iterates through the tokens. If the token is an operator, it pops the top two numbers from the stack, performs the operation, and pushes the result back onto the stack. If the token is a number, it pushes it onto the stack.

--- solution.ts
+++ solution.ts
@@ -1,4 +1,22 @@
-function evaluateRPN(tokens: string[]): number {
-    let stack: number[] = [];
-    let operators: string[] = ["+", "-", "*", "/"];
-}
+for (let token of tokens) {
+        if (operators.includes(token)) {
+            let num2 = stack.pop() as number;
+            let num1 = stack.pop() as number;
+            switch (token) {
+                case "+":
+                    stack.push(num1 + num2);
+                    break;
+                case "-":
+                    stack.push(num1 - num2);
+                    break;
+                case "*":
+                    stack.push(num1 * num2);
+                    break;
+                case "/":
+                    stack.push(num1 / num2);
+                    break;
+            }
+        } else {
+            stack.push(Number(token));
+        }
+    }
