d449d8693746eba0ec65655ae22a2a58a1e6cb98
Kooreis <kooreis.official@gmail.com>
2022-05-29 12:39:58 -0500

    In this part, if the token is not a number, it is considered an operator. The script pops two numbers from the stack and performs the addition operation if the operator is '+'. The result is then pushed back onto the stack.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,7 @@
-function evaluateRPN(tokens) {
-    let stack = [];
-    for (let token of tokens) {
-        if (!isNaN(parseInt(token))) {
-            stack.push(parseInt(token));
-        }
+else {
+            let num2 = stack.pop();
+            let num1 = stack.pop();
+            switch (token) {
+                case "+":
+                    stack.push(num1 + num2);
+                    break;
